bd487503f1c39c592aeebc2baf3e55c045234e43
sirancheevi <127745333+Sirancheevi29@users.noreply.github.com>
2023-04-04 14:12:37 +0200

    Initial setup

---
+++ kafka-csharp- core-api/kafka-csharp- core-api/Controllers/MoviesController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using kafka_csharp__core_api.Model;
+
+namespace kafka_csharp__core_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MoviesController : ControllerBase
+    {
+        [HttpGet]
+        [Consumes("application/json")]
+        [Route("movies")]
+        [ProducesResponseType(typeof(List<Movies>), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public ActionResult<List<Movies>> Get()
+        {
+            try
+            {
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+
+        }
+    }
+}
