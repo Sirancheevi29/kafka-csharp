ae0a98968cc4aef5d224a0d0530f891f978c4244
sirancheevi <127745333+Sirancheevi29@users.noreply.github.com>
2023-04-04 22:38:45 +0200

    final commit

--- kafka-csharp- core-api/kafka-csharp- core-api/Controllers/MoviesController.cs
+++ kafka-csharp- core-api/kafka-csharp- core-api/Controllers/MoviesController.cs
@@ -1,29 +1,54 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using kafka_csharp__core_api.Model;
+using kafka_csharp__core_api.Services;
+using System.Text.Json.Serialization;
+using Swashbuckle.AspNetCore.Swagger;
+using Newtonsoft.Json;
 
 namespace kafka_csharp__core_api.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/kafka/[controller]")]
     [ApiController]
     public class MoviesController : ControllerBase
     {
         [HttpGet]
-        [Consumes("application/json")]
-        [Route("movies")]
         [ProducesResponseType(typeof(List<Movies>), StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public ActionResult<List<Movies>> Get()
         {
             try
             {
-                return Ok();
+                KakfaCloud kakfa = new KakfaCloud();
+                List<Movies> movies = kakfa.Consumer("movies");
+
+                return Ok(movies);
             }
             catch (Exception ex)
             {
                 return BadRequest(ex.Message);
             }
 
+        }
+
+        [HttpPost]
+        [Consumes("application/json")]
+        [ProducesResponseType(typeof(Movies), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public ActionResult<Movies> Post(List<Movies> movies)
+        {
+            try
+            {
+
+                KakfaCloud kakfa = new KakfaCloud();
+                kakfa.Producer("movies", movies);
+
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
     }
 }
---
+++ kafka-csharp- core-api/kafka-csharp- core-api/Services/KakfaCloud.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Diagnostics;
+using System.Net;
+using Confluent.Kafka;
+using kafka_csharp__core_api.Model;
+using Newtonsoft.Json;
+using Swashbuckle.AspNetCore.Swagger;
+
+namespace kafka_csharp__core_api.Services
+{
+    public class KakfaCloud
+    {
+        public ProducerConfig _producerConfig { get; set; }
+        public ConsumerConfig _consumerConfig { get; set; }
+        public KakfaCloud()
+        {
+            _producerConfig = new ProducerConfig
+            {
+                BootstrapServers = "",
+                ClientId = Dns.GetHostName(),
+                SaslMechanism = SaslMechanism.Plain,
+                SaslUsername = "",
+                SaslPassword = "",
+                SecurityProtocol = SecurityProtocol.SaslSsl,
+                Acks = Acks.Leader
+            };
+
+            _consumerConfig = new ConsumerConfig
+            {
+                BootstrapServers = "",
+                ClientId = Dns.GetHostName(),
+                SaslMechanism = SaslMechanism.Plain,
+                SaslUsername = "",
+                SaslPassword = "",
+                SecurityProtocol = SecurityProtocol.SaslSsl,
+                GroupId = "movies_group",
+                AutoOffsetReset = AutoOffsetReset.Earliest
+            };
+        }
+
+        public bool Producer(string topic, List<Movies> movies)
+        {
+            try
+            {
+                using (var producer = new ProducerBuilder<Null, string>(_producerConfig).Build())
+                {
+                    producer.Produce(topic, new Message<Null, string> { Timestamp = new Timestamp(DateTime.Now), Value = JsonConvert.SerializeObject(movies) });
+                }
+
+                return true;
+            }
+            catch (ProduceException<Null, string> ex)
+            {
+                return false;
+            }
+        }
+
+        public List<Movies> Consumer(string topic)
+        {
+            List<Movies> movies = new List<Movies>();
+            try
+            {
+                using (var consumerBuilder = new ConsumerBuilder
+                <Ignore, string>(_consumerConfig).Build())
+                {
+                    consumerBuilder.Subscribe(topic);
+                    var cancelToken = new CancellationTokenSource();
+
+                    try
+                    {
+                        while (true)
+                        {
+                            var consumer = consumerBuilder.Consume
+                               (cancelToken.Token);
+                            movies = JsonConvert.DeserializeObject<List<Movies>>(consumer.Message.Value);
+                            
+                        }
+                    }
+                    catch (OperationCanceledException)
+                    {
+                        consumerBuilder.Close();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine(ex.Message);
+            }
+            return movies;
+        }
+
+    }
+}
